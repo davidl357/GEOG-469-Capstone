4ad72cb3e704e9c82553c26e0339defdfbfe652b
David Lee <davidjlee168@hotmail.com>
2019-04-25 10:04:47 -0700

    fixed link to urls in main map and removed url from auburn map

--- Auburn_food_bank_locations.js
+++ Auburn_food_bank_locations.js
@@ -3,6 +3,6 @@ var foodBankLocations = {
         "name": "food_back_locations",
             "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
     "features": [
-        { "type": "Feature", "properties": { "name": " <br> Auburn", "address": " <br> 930 18th Pl NE Auburn, WA 98002", "url": " <br> http://www.theauburnfoodbank.org/", "hours": " <br> Mon-Tu-Th-Fri 9:00am - 1:15pm  <br> 2nd Wed 4:30pm - 6:30pm  <br> 2nd Sat Senior Home Delivery  <br> Mon/Fri Dinner  <br> Day Shelter: M-F 10:00am - 3:30pm", "email": " <br> debbiec@theauburnfoodbank.org", "phone": " <br> 253-833-8925", "y": 47.3237, "x": -122.21828 }, "geometry": { "type": "Point", "coordinates": [-122.21828, 47.3237] } }
+        { "type": "Feature", "properties": { "name": " <br> Auburn", "address": " <br> 930 18th Pl NE Auburn, WA 98002", "hours": " <br> Mon-Tu-Th-Fri 9:00am - 1:15pm  <br> 2nd Wed 4:30pm - 6:30pm  <br> 2nd Sat Senior Home Delivery  <br> Mon/Fri Dinner  <br> Day Shelter: M-F 10:00am - 3:30pm", "email": " <br> debbiec@theauburnfoodbank.org", "phone": " <br> 253-833-8925", "y": 47.3237, "x": -122.21828 }, "geometry": { "type": "Point", "coordinates": [-122.21828, 47.3237] } }
     ]
 }

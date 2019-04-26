8a5845dd368676c9a5fb785c9f24befe8ab9ccc7
David Lee <davidjlee168@hotmail.com>
2019-04-26 13:36:19 -0700

    created maple valley boundary

---
+++ Google Map/Maple Valley Map/Maple_Valley_food_bank_locations.js
@@ -0,0 +1,8 @@
+var foodBankLocations = {
+    "type": "FeatureCollection",
+    "name": "food_back_locations",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+        { "type": "Feature", "properties": { "name": " <br> Maple Valley", "address": " <br> 21415 Renton Maple Valley Rd SE Maple Valley, WA 98038", "url": "https://maplevalleyfoodbank.org/", "hours": " <br> Tues-Wed-Th 10:30 - 1:00  <br> 2nd & 4th Tues  6:00 - 7:30 pm", "email": " <br> dan@maplevalleyfoodbank.org", "phone": " <br> 425-432-8633 ext 103", "y": 47.4095, "x": -122.03952 }, "geometry": { "type": "Point", "coordinates": [-122.03952, 47.4095] } },
+    ]
+}

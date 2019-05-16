eb4132c416b805dd3a228ad4fcd4e6e70d38e7fa
David Lee <davidjlee168@hotmail.com>
2019-05-16 14:02:00 -0700

    added transit routes for all maps

--- Google Map/Kent Map/Kent_food_bank_boundaries.js
+++ Google Map/Kent Map/Kent_food_bank_boundaries.js
@@ -1,4 +1,4 @@
-var = foodBankBoundaries = {
+var foodBankBoundaries = {
 "type": "FeatureCollection",
 "name": "kent_boundary",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
